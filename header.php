97de156c75a25fda1792aff568e12e1fe26ab50e
theyaregray <150846073+theyaregray@users.noreply.github.com>
2024-03-27 14:11:23 -0700

    Updated entry-title and page-title

--- header.php
+++ header.php
@@ -33,15 +33,6 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
 			$floral_shop_description = get_bloginfo( 'description', 'display' );
 			if ( $floral_shop_description || is_customize_preview() ) :
 				?>
